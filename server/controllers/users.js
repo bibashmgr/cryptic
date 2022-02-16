ac632ab774aff2c461fb6e6caa977bf706ead4fd
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-02-16 22:51:01 +0545

    Making some changes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 import Users from '../models/users.js';
 
 export const getUsers = async (req, res) => {
@@ -13,6 +15,9 @@ export const registerUser = async (req, res) => {
   const user = req.body;
   const newUser = new Users(user);
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(newUser.password, salt);
+    newUser.password = hashPassword;
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
