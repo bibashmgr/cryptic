70071d88097a556c6ac2d6d0a2c982a7d91db543
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-03-15 00:15:41 +0545

    Adding states

--- client/src/components/MiddleBar/index.jsx
+++ client/src/components/MiddleBar/index.jsx
@@ -10,14 +10,16 @@ import {
   GridLogo,
 } from './MiddleBarElements';
 
-const MiddleBar = () => {
+const MiddleBar = ({ setIsOpen }) => {
   const handlePostBtn = () => {
+    setIsOpen(true);
     document.getElementById('mypost').style.borderBottom =
       '2.5px solid #78bcff';
     document.getElementById('mysave').style.borderBottom = 'none';
   };
 
   const handleSaveBtn = () => {
+    setIsOpen(false);
     document.getElementById('mypost').style.borderBottom = 'none';
     document.getElementById('mysave').style.borderBottom =
       '2.5px solid #78bcff';
