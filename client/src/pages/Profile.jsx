f3d41f536a1f5f71d5e6351c20e0fd10b926a829
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-03-15 00:17:37 +0545

    Adding myPost & savedPost features to Profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 // scss
 import '../styles/Main.scss';
@@ -9,15 +12,39 @@ import TopBar from '../components/TopBar';
 import NavBar from '../components/NavBar';
 import ProfileCard from '../components/ProfileCard';
 import MiddleBar from '../components/MiddleBar';
+import Posts from '../components/Posts';
 
 const Profile = () => {
+  // variables:
+  const loginUser = localStorage.getItem('loginUser');
+
+  // states:
+  const [isOpen, setIsOpen] = useState(true);
+  const [myPosts, setMyPosts] = useState([]);
+  const [savedPosts, setSavedPosts] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/posts/myposts/${loginUser}`)
+      .then((res) => res.data)
+      .then((data) => setMyPosts(data));
+  }, [loginUser]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/posts/savedposts/${loginUser}`)
+      .then((res) => res.data)
+      .then((data) => setSavedPosts(data));
+  }, [loginUser]);
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <div className='main-container'>
         <div className='sub-container'>
           <div className='profile-container'>
             <ProfileCard />
-            <MiddleBar />
+            <MiddleBar setIsOpen={setIsOpen} />
+            <Posts posts={isOpen ? myPosts : savedPosts} />
           </div>
         </div>
       </div>
