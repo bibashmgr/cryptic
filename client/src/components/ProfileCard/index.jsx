28a670fd4734084ffce0c7cc4ff9bf9162ff42ee
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-03-14 16:03:52 +0545

    Fetching data on Profile card

--- client/src/components/ProfileCard/index.jsx
+++ client/src/components/ProfileCard/index.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 // elements
 import { Card, CardAvatar, CardUsername } from './ProfileCardElements';
 
 const ProfileCard = () => {
+  // state:
+  const [currentUser, setCurrentUser] = useState('');
+
+  useEffect(() => {
+    const loginUser = localStorage.getItem('loginUser');
+    axios
+      .get(`http://localhost:8080/api/users/${loginUser}`)
+      .then((res) => res.data)
+      .then((data) => setCurrentUser(data));
+  }, []);
   return (
     <Card>
-      <CardAvatar src='/images/avatar.jpg' />
-      <CardUsername>@keepitlow</CardUsername>
+      <CardAvatar src='/images/default_avatar.jpg' />
+      <CardUsername>{currentUser.username}</CardUsername>
     </Card>
   );
 };
