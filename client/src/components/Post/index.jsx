7d3d9b6a2f12c1384545d842407c39cfa33a99b2
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-03-14 23:17:54 +0545

    Using moment.js format

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 import axios from 'axios';
-import * as timeago from 'timeago.js';
+import moment from 'moment';
 
 // elements
 import {
@@ -25,18 +25,18 @@ import {
 const Post = ({ post }) => {
   const [username, setUsername] = useState('');
   useEffect(() => {
-    const user = axios
+    axios
       .get(`http://localhost:8080/api/users/${post.userId}`)
       .then((res) => res.data)
       .then((data) => setUsername(data.username));
-  }, []);
+  }, [post.userId]);
   return (
     <Card>
       <CardHeader>
         <CardAvatar src='/images/default_avatar.jpg' />
         <CardHeaderText>
           <CardUserName>{username}</CardUserName>
-          <CardDate>{timeago.format(post.createdAt)}</CardDate>
+          <CardDate>{moment(post.createdAt).fromNow()}</CardDate>
         </CardHeaderText>
       </CardHeader>
       <CardContent>
