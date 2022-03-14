8606a1e62333b509a94f533502f8998496a4cb53
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-03-15 00:16:49 +0545

    Fixing routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -17,6 +17,6 @@ router.get('/', getPosts);
 router.put('/:id/like', likePost);
 router.put('/:id/save', savePost);
 router.get('/myposts/:id', getMyPosts);
-router.get('/saved/:id', getSavedPosts);
+router.get('/savedposts/:id', getSavedPosts);
 
 export default router;
