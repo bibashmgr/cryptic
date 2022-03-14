441c645badf7a156831842337dd913c3105597fc
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-03-15 00:15:59 +0545

    Cleaning up

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 import axios from 'axios';
 import moment from 'moment';
