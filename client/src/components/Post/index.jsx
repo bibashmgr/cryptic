8048e8bdf00b8d7a228703043ab5c35c4b653a74
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-03-21 22:16:52 +0545

    Adding like & save features

--- client/src/components/Post/index.jsx
+++ client/src/components/Post/index.jsx
@@ -13,23 +13,66 @@ import {
   CardDate,
   CardHeader,
   CardHeaderText,
-  LikeCount,
+  LikeCounter,
   CardText,
   CardUserName,
   LikeButton,
-  LikeLogo,
   SaveButton,
-  SaveLogo,
 } from './PostElements';
 
+// icons
+import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
+import { BsBookmark, BsBookmarkFill } from 'react-icons/bs';
+
 const Post = ({ post }) => {
+  // variables:
+  const loginUser = localStorage.getItem('loginUser');
+
+  // styles:
+  const logoStyle = {
+    fontSize: '1.5rem',
+    fontWeight: 700,
+    color: '#78bcff',
+  };
+
+  // states:
   const [username, setUsername] = useState('');
+  const [likeCount, setLikeCount] = useState(post.likes.length);
+  const [isLiked, setIsLiked] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
+
+  useEffect(() => {
+    setIsLiked(post.likes.includes(loginUser));
+    setIsSaved(post.saved.includes(loginUser));
+  }, [loginUser, post.likes, post.saved]);
+
   useEffect(() => {
     axios
       .get(`http://localhost:8080/api/users/${post.userId}`)
       .then((res) => res.data)
       .then((data) => setUsername(data.username));
   }, [post.userId]);
+
+  // handlers:
+  const handleLike = () => {
+    axios
+      .put(`http://localhost:8080/api/posts/${post._id}/like`, {
+        userId: loginUser,
+      })
+      .then((res) => console.log(res));
+    setLikeCount(isLiked ? likeCount - 1 : likeCount + 1);
+    setIsLiked(!isLiked);
+  };
+
+  const handleSave = (e) => {
+    axios
+      .put(`http://localhost:8080/api/posts/${post._id}/save`, {
+        userId: loginUser,
+      })
+      .then((res) => console.log(res));
+    setIsSaved(!isSaved);
+  };
+
   return (
     <Card>
       <CardHeader>
@@ -43,12 +86,20 @@ const Post = ({ post }) => {
         <CardText>{post.desc}</CardText>
       </CardContent>
       <CardButtons>
-        <LikeButton>
-          <LikeCount>{post.likes.length}</LikeCount>
-          <LikeLogo />
+        <LikeButton onClick={handleLike}>
+          <LikeCounter>{likeCount}</LikeCounter>
+          {isLiked ? (
+            <AiFillLike style={logoStyle} />
+          ) : (
+            <AiOutlineLike style={logoStyle} />
+          )}
         </LikeButton>
-        <SaveButton>
-          <SaveLogo />
+        <SaveButton onClick={handleSave}>
+          {isSaved ? (
+            <BsBookmarkFill style={logoStyle} />
+          ) : (
+            <BsBookmark style={logoStyle} />
+          )}
         </SaveButton>
       </CardButtons>
     </Card>
