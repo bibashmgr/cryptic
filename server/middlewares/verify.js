ec67489419ecb9ec497e1d6c527295372a6a5a0d
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-10 02:32:33 +0545

    Adding middlewares

---
+++ server/middlewares/verify.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ACCESS_TOKEN = process.env.SECRET_ACCESS_TOKEN;
+
+export const getVerify = (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, ACCESS_TOKEN);
+      req.userId = decoded.id;
+      next();
+    } catch (error) {
+      res.status(403).json('ACCESS DENIED');
+    }
+  } else {
+    res.status(401).json('TOKEN NOT FOUND');
+  }
+};
